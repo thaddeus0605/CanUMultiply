7b850552c05f17527f8919efa420b13ffc5b4de7
thaddeus0605 <tdronski@gmail.com>
2022-12-19 18:16:31 -0600

    Initial Commit

---
+++ CanUMultiply/CanUMultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanUMultiplyApp.swift
+//  CanUMultiply
+//
+//  Created by Thaddeus Dronski on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CanUMultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
